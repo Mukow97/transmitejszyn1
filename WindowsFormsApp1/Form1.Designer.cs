28958032e5ac6c59a397d6174c7243e2be237104
Mukow97 <40125860+Mukow97@users.noreply.github.com>
2018-06-17 22:43:01 +0200

    ukonczone wykresy bodego i poprawione wzory do y

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.b1 = new System.Windows.Forms.TextBox();
             this.b0 = new System.Windows.Forms.TextBox();
@@ -55,7 +57,6 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
             this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(371, 187);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -64,55 +65,49 @@
             // 
             // b1
             // 
-            this.b1.Location = new System.Drawing.Point(193, 73);
-            this.b1.Margin = new System.Windows.Forms.Padding(4);
+            this.b1.Location = new System.Drawing.Point(145, 59);
             this.b1.Name = "b1";
-            this.b1.Size = new System.Drawing.Size(33, 22);
+            this.b1.Size = new System.Drawing.Size(26, 20);
             this.b1.TabIndex = 1;
             this.b1.Tag = "pole";
             // 
             // b0
             // 
-            this.b0.Location = new System.Drawing.Point(275, 73);
-            this.b0.Margin = new System.Windows.Forms.Padding(4);
+            this.b0.Location = new System.Drawing.Point(206, 59);
             this.b0.Name = "b0";
-            this.b0.Size = new System.Drawing.Size(31, 22);
+            this.b0.Size = new System.Drawing.Size(24, 20);
             this.b0.TabIndex = 2;
             this.b0.Tag = "pole";
             // 
             // txta1
             // 
-            this.txta1.Location = new System.Drawing.Point(292, 123);
-            this.txta1.Margin = new System.Windows.Forms.Padding(4);
+            this.txta1.Location = new System.Drawing.Point(219, 100);
             this.txta1.Name = "txta1";
-            this.txta1.Size = new System.Drawing.Size(27, 22);
+            this.txta1.Size = new System.Drawing.Size(21, 20);
             this.txta1.TabIndex = 4;
             this.txta1.Tag = "pole";
             // 
             // txta2
             // 
-            this.txta2.Location = new System.Drawing.Point(215, 123);
-            this.txta2.Margin = new System.Windows.Forms.Padding(4);
+            this.txta2.Location = new System.Drawing.Point(161, 100);
             this.txta2.Name = "txta2";
-            this.txta2.Size = new System.Drawing.Size(29, 22);
+            this.txta2.Size = new System.Drawing.Size(23, 20);
             this.txta2.TabIndex = 5;
             this.txta2.Tag = "pole";
             // 
             // txta3
             // 
-            this.txta3.Location = new System.Drawing.Point(147, 123);
-            this.txta3.Margin = new System.Windows.Forms.Padding(4);
+            this.txta3.Location = new System.Drawing.Point(110, 100);
             this.txta3.Name = "txta3";
-            this.txta3.Size = new System.Drawing.Size(25, 22);
+            this.txta3.Size = new System.Drawing.Size(20, 20);
             this.txta3.TabIndex = 6;
             this.txta3.Tag = "pole";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(395, 13);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(312, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 28);
+            this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 7;
             this.button1.Text = "sprawdź stabilność";
             this.button1.UseVisualStyleBackColor = true;
@@ -121,24 +116,25 @@
             // stabilnosc
             // 
             this.stabilnosc.AutoSize = true;
-            this.stabilnosc.Location = new System.Drawing.Point(533, 91);
-            this.stabilnosc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.stabilnosc.Location = new System.Drawing.Point(400, 74);
             this.stabilnosc.Name = "stabilnosc";
-            this.stabilnosc.Size = new System.Drawing.Size(0, 17);
+            this.stabilnosc.Size = new System.Drawing.Size(0, 13);
             this.stabilnosc.TabIndex = 15;
             // 
             // txta0
             // 
-            this.txta0.Location = new System.Drawing.Point(360, 123);
+            this.txta0.Location = new System.Drawing.Point(270, 100);
+            this.txta0.Margin = new System.Windows.Forms.Padding(2);
             this.txta0.Name = "txta0";
-            this.txta0.Size = new System.Drawing.Size(33, 22);
+            this.txta0.Size = new System.Drawing.Size(26, 20);
             this.txta0.TabIndex = 17;
             // 
             // sym
             // 
-            this.sym.Location = new System.Drawing.Point(12, 311);
+            this.sym.Location = new System.Drawing.Point(12, 289);
+            this.sym.Margin = new System.Windows.Forms.Padding(2);
             this.sym.Name = "sym";
-            this.sym.Size = new System.Drawing.Size(106, 28);
+            this.sym.Size = new System.Drawing.Size(80, 23);
             this.sym.TabIndex = 18;
             this.sym.Text = "Symuluj układ";
             this.sym.UseVisualStyleBackColor = true;
@@ -146,24 +142,35 @@
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
-            this.chart1.Location = new System.Drawing.Point(203, 193);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(249, 164);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series5.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
-            series5.Legend = "Legend1";
-            series5.Name = "y";
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series6.Legend = "Legend1";
-            series6.Name = "u(t)";
-            this.chart1.Series.Add(series5);
-            this.chart1.Series.Add(series6);
-            this.chart1.Size = new System.Drawing.Size(1331, 815);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series1.Legend = "Legend1";
+            series1.Name = "y";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "u(t)";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "A(w)";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "fi(w)";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(910, 453);
             this.chart1.TabIndex = 19;
             this.chart1.Text = "chart1";
             // 
@@ -173,27 +180,30 @@
             this.checkedListBox1.Items.AddRange(new object[] {
             "sinusoidalnym",
             "trójkątnym",
-            "prostokatnym"});
-            this.checkedListBox1.Location = new System.Drawing.Point(15, 233);
+            "prostokatnym",
+            "Bodego"});
+            this.checkedListBox1.Location = new System.Drawing.Point(11, 189);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(135, 72);
+            this.checkedListBox1.Size = new System.Drawing.Size(102, 64);
             this.checkedListBox1.TabIndex = 22;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 202);
+            this.label1.Location = new System.Drawing.Point(9, 164);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 23;
             this.label1.Text = "Pobudzenie sygnałem";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.ClientSize = new System.Drawing.Size(1203, 716);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.chart1);
@@ -207,7 +217,6 @@
             this.Controls.Add(this.b0);
             this.Controls.Add(this.b1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -20,7 +20,11 @@ namespace WindowsFormsApp1
         SortedList utSL = new SortedList();
         SortedList utpSL = new SortedList();
         SortedList uttSL = new SortedList();
+        SortedList dutSL = new SortedList();
+        SortedList dutpSL = new SortedList();
+        SortedList duttSL = new SortedList();
         double ut;
+        double dut;
 
 
         private void button1_Click(object sender, EventArgs e)
@@ -85,9 +89,12 @@ namespace WindowsFormsApp1
         private void sym_Click(object sender, EventArgs e)
         {
             double dt, x1 = 0, x2 = 0, x3 = 0, x4 = 0, t = 0, y = 0, a1, a2, a3, a0, db1, db0;
-            int j, i;           
+            double dw, w = 0, A = 0, fi = 0, w1 = 0, w2 = 0, w3 = 0, m = 0, l = 0;
+            int j, i, jw;           
             j = 5000;
             dt = 0.002;
+            jw = 200;
+            dw = 0.05;
             a1 = Convert.ToDouble(txta1.Text);
             a2 = Convert.ToDouble(txta2.Text);
             a3 = Convert.ToDouble(txta3.Text);
@@ -99,7 +106,9 @@ namespace WindowsFormsApp1
             SortedList x3SL = new SortedList();
             SortedList x4SL = new SortedList();
             SortedList ySL = new SortedList();
-            string[] s = { "sinusoidalnym", "trójkątnym", "prostokątnym" };
+            SortedList ASL = new SortedList();
+            SortedList fiSL = new SortedList();
+            string[] s = { "sinusoidalnym", "trójkątnym", "prostokątnym", "Bodego" };
             foreach (var series in chart1.Series)
             {
                 series.Points.Clear();
@@ -109,25 +118,28 @@ namespace WindowsFormsApp1
                     if (item.ToString() == "sinusoidalnym")
                     {
                         ut = Convert.ToDouble(utSL[t]);
+                        dut = Convert.ToDouble(dutSL[t]);
                     }
                     else
                     {
                         if (item.ToString() == "trójkątnym")
                             {
                                 ut = Convert.ToDouble(uttSL[t]);
-                            }
+                                dut = Convert.ToDouble(duttSL[t]);
+                        }
                             else
                             {
                                 if (item.ToString() == "prostokatnym")
                                     {
                                         ut = Convert.ToDouble(utpSL[t]);
+                                        dut = 0;
                                     }
                             }
                     }
                 x1 = x1 + x2 * dt;
                 x2 = x2 + x3 * dt;
                 x3 = x3 + x4 * dt;
-                x4 = x4 + (-a0 * x1 - a1 * x2 - a2 * x3 - a3 * x4 + db1 * ut + db0 * ut) * dt;
+                x4 = x4 + (-a0 * x1 - a1 * x2 - a2 * x3 - a3 * x4 + db1 * dut + db0 * ut) * dt;
                 y = x1;
                 x1SL.Add(t, x1);
                 x2SL.Add(t, x2);
@@ -138,6 +150,27 @@ namespace WindowsFormsApp1
                 chart1.Series["u(t)"].Points.AddXY(t, ut);
                 t = t + dt;
             }
+            for(i=0; i<jw; i++)
+            {
+                foreach (Object item in checkedListBox1.CheckedItems)
+                    if (item.ToString() == "Bodego")
+                    {
+                        w1 = w1 + db1 * w;
+                        w2 = w2 + w * w * w * w - a2 * w * w + a0;
+                        w3 = w3 + a1 * w - a3 * w * w * w;
+                        l = w1 * w1 + db0 * db0;
+                        m = w2 * w2 + w3 * w3;
+                        A = Math.Sqrt(l) / Math.Sqrt(m);
+                        fi = Math.Atan2(w1, db0) - Math.Atan2(w2, w3);
+                        ASL.Add(w, A);
+                        fiSL.Add(w, fi);
+                        chart1.Series["A(w)"].Points.AddXY(w, ASL[w]);
+                        chart1.Series["fi(w)"].Points.AddXY(w, fiSL[w]);
+                        w = w + dw;
+                    }
+                   
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -147,6 +180,7 @@ namespace WindowsFormsApp1
             for (i = 0; i < j; i++)
             {
                 utSL.Add(t, Math.Sin(t));
+                dutSL.Add(t, Math.Cos(t));
                 t = t + dt;
             }
             for (i = 0; i < j; i++)
@@ -163,6 +197,7 @@ namespace WindowsFormsApp1
             }
             double value = 0;
             int marker = 0;
+            int pochodna = 0;
             for (i = 0; i < j; i++)
             {
                 if (marker == 0)
@@ -170,10 +205,12 @@ namespace WindowsFormsApp1
                     if (value > -1.1 && value < 1)
                     {
                         value = value+dt;
+                        pochodna = 1;
                     }
                     else
                     {
                         marker = 1;
+                        pochodna = 0;
                     }
                 }
                 else
@@ -181,16 +218,22 @@ namespace WindowsFormsApp1
                     if(value>-1 && value < 1.1)
                     {
                         value = value - dt;
+                        pochodna = -1;
                     }
                     else
                     {
                         marker = 0;
+                        pochodna = 0;
                     }
                 }
                 uttSL.Add(r, value);
+                duttSL.Add(r, pochodna);
                 r = r + dt;
             }   
                
-            }
         }
+
+    }
+
+
 }
